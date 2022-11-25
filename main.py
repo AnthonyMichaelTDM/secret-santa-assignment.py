b1fca0191f55eb973a52564d9a8927a8e6f69f83
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2022-11-25 13:49:50 -0800

    name extraction

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-name_file = "./names.txt"
+#!/usr/bin/env python3
+
+
+def get_names(file: str) -> list[str]:
+    names: list[str] = []
+    with open(file, "r") as f:
+        # extract names
+        names = [str(name).strip() for name in f.readlines()]
+
+        # prune empty elements
+        names = [name for name in names if name != ""]
+
+    # return names
+    return names
+
+
+def get_assignments(names: list[str]) -> dict[str, str]:
+    pass
 
 
 def main():
-    names: list[str] = []
+    # get names from names.txt
+    names: list[str] = get_names("./names.txt")
 
-    with open(name_file, "r") as f:
-        pass
+    # get assignments from names
+    assignments: dict[str, str] = get_assignments(names)
+
+    # print assignments in a nice format
+
     pass
 
 
